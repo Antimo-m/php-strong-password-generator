b14e77556ff677e07329f3fad9c645a35695b511
Antimo-M <montellaantimo@live.it>
2026-03-16 15:47:06 +0100

    milestone 2 : sposatare la logica in un file function.php e poi includerla. Fatto.

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+function generapassword($pass = 12)
+{
+    $minuscole = 'abcdefghijklmnopqrstuvwxyz';
+    $maiuscole = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $numeri = '0123456789';
+    $simboli = '!@#$%^&*()-_=+[]{};:,.<>?';
+
+    $tutti = $minuscole . $maiuscole . $numeri . $simboli;
+
+
+    $password = '';
+    for ($i = 0; $i < $pass; $i++) {
+        $posizione = random_int(0, strlen($tutti) - 1);
+        $password .= $tutti[$posizione];
+    }
+    return $password;
+}
+
+$pass = isset($_GET["pass"]) ? intval($_GET['pass']) : 12;
+$passwordGenerata = '';
+
+if ($pass > 0) {
+    $passwordGenerata = generapassword($pass);
+}
--- index.php
+++ index.php
@@ -1,30 +1,6 @@
  <?php
-    function generapassword($pass = 12)
-    {
-        $minuscole = 'abcdefghijklmnopqrstuvwxyz';
-        $maiuscole = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $numeri = '0123456789';
-        $simboli = '!@#$%^&*()-_=+[]{};:,.<>?';
-
-        $tutti = $minuscole . $maiuscole . $numeri . $simboli;
-
-
-        $password = '';
-        for ($i = 0; $i < $pass; $i++) {
-            $posizione = random_int(0, strlen($tutti)- 1);
-            $password .= $tutti[$posizione];
-        }
-        return $password;
-    }
-
-    $pass = isset($_GET["pass"]) ? intval($_GET['pass']) : 12;
-    $passwordGenerata = '';
-
-    if ($pass > 0) {
-        $passwordGenerata = generapassword($pass);
-    }
-    ?>
-
+ include_once "./functions.php"
+ ?>
  <!DOCTYPE html>
  <html lang="en">
 
